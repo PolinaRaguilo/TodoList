8da7152581f265a398f4f59875ce6ee408a92bc2
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-04-19 15:12:48 +0300

    Added state for ToDo

--- src/components/card-todo/Card-todo.js
+++ src/components/card-todo/Card-todo.js
@@ -1,4 +1,19 @@
-import { Card, CardContent, makeStyles, Typography } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardContent,
+  colors,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  makeStyles,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import { useState } from 'react';
 
 const useStyles = makeStyles({
   root: {
@@ -9,23 +24,118 @@ const useStyles = makeStyles({
   title: {
     fontSize: 14,
   },
+  description__wrapper: {
+    padding: 0,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  btn__change: {
+    minWidth: 100,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  btn__modal: {
+    backgroundColor: colors.grey[800],
+    color: colors.grey[100],
+  },
+  todo: {
+    backgroundColor: '#ffcdd2',
+  },
+  inProgress: {
+    backgroundColor: '#64ffda',
+  },
+  done: {
+    backgroundColor: '#bdbdbd',
+  },
 });
 
-const CardTodo = () => {
+const CardTodo = ({ items, setItems, item }) => {
   const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+  const [stateSelect, setState] = useState('');
+
+  const onChangeSelect = (e) => {
+    setState(e.target.value);
+  };
+
+  const onOpenHandler = () => {
+    setOpen(true);
+  };
+
+  const onCloseHandler = () => {
+    setOpen(false);
+  };
+
+  const changeStateItem = () => {
+    const newItems = items.map((todo) => {
+      if (todo.id === item.id) {
+        return { ...todo, state: stateSelect };
+      }
+      return todo;
+    });
+    setItems(newItems);
+    setOpen(false);
+  };
+
   return (
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Header
-        </Typography>
-        <Typography variant="body2" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography>
-      </CardContent>
-    </Card>
+    <>
+      <Card
+        className={`${classes.root} ${
+          (item.state === 'todo' && classes.todo) ||
+          (item.state === 'in-progress' && classes.inProgress) ||
+          (item.state === 'done' && classes.done)
+        }`}
+        variant="outlined"
+      >
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {item.title}
+          </Typography>
+          <Container className={classes.description__wrapper}>
+            <Typography variant="body2" component="p">
+              {item.description}
+            </Typography>
+            <Button className={classes.btn__change} onClick={onOpenHandler}>
+              State
+            </Button>
+          </Container>
+        </CardContent>
+      </Card>
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        open={open}
+        onClose={onCloseHandler}
+      >
+        <DialogTitle>Select state </DialogTitle>
+        <DialogContent>
+          <form className={classes.container}>
+            <FormControl className={classes.formControl}>
+              <Select native value={stateSelect} onChange={onChangeSelect}>
+                <option value="todo">ToDo</option>
+                <option value="in-progress">In progress</option>
+                <option value="done">Done</option>
+              </Select>
+            </FormControl>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={onCloseHandler}
+            className={`${classes.btn__change} ${classes.btn__modal}`}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={changeStateItem}
+            className={`${classes.btn__change} ${classes.btn__modal}`}
+          >
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 export default CardTodo;
--- src/components/main-page/Main-page.js
+++ src/components/main-page/Main-page.js
@@ -1,4 +1,5 @@
 import { Container, makeStyles, Typography } from '@material-ui/core';
+import { useState } from 'react';
 import AddForm from '../add-form/add-form';
 import CardTodo from '../card-todo/Card-todo';
 
@@ -12,14 +13,37 @@ const useStyles = makeStyles(() => ({
 
 const MainPage = () => {
   const classes = useStyles();
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      title: 'fdsf',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+      state: 'todo',
+    },
+    {
+      id: 2,
+      title: '231',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.Aenean massa.Aenean massa.Aenean massa.',
+      state: 'todo',
+    },
+  ]);
+
   return (
     <>
       <Typography className={classes.main__title}>ToDo List</Typography>
       <Container>
-        <CardTodo />
-        <CardTodo />
-        <CardTodo />
-        <CardTodo />
+        {items.map((item) => {
+          return (
+            <CardTodo
+              key={item.id}
+              item={item}
+              setItems={setItems}
+              items={items}
+            />
+          );
+        })}
       </Container>
       <AddForm />
     </>
