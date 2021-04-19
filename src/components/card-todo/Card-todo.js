b8aef3bdb90f3b7df89256bdc1844a209a4f3a4c
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-04-19 12:10:59 +0300

    Created function to add new ToDo

--- src/components/card-todo/Card-todo.js
+++ src/components/card-todo/Card-todo.js
@@ -11,18 +11,16 @@ const useStyles = makeStyles({
   },
 });
 
-const CardTodo = () => {
+const CardTodo = ({ todo }) => {
   const classes = useStyles();
   return (
     <Card className={classes.root} variant="outlined">
       <CardContent>
         <Typography variant="h5" component="h2">
-          Header
+          {todo.title}
         </Typography>
         <Typography variant="body2" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {todo.description}
         </Typography>
       </CardContent>
     </Card>
