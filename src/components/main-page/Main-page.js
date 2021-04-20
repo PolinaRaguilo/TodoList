3ae8a9c19d4878ca8cd4a17d8245c7244dd37d4f
Polina Raguilo <Palina.Rahila@itechart-group.com>
2021-04-20 14:25:02 +0300

    Added sorting

--- src/components/main-page/Main-page.js
+++ src/components/main-page/Main-page.js
@@ -1,4 +1,10 @@
-import { Container, makeStyles, Typography } from '@material-ui/core';
+import {
+  Container,
+  makeStyles,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
 import { useState } from 'react';
 import AddForm from '../add-form/add-form';
 import CardTodo from '../card-todo/Card-todo';
@@ -8,23 +14,76 @@ const useStyles = makeStyles(() => ({
     fontSize: 50,
     textAlign: 'center',
     marginBottom: 20,
+    marginRight: 100,
+  },
+  wrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  selectState: {
+    width: 200,
+    height: 40,
+    marginRight: 45,
   },
 }));
 
 const MainPage = () => {
   const classes = useStyles();
 
+  const [currentState, setCurrentState] = useState('All');
+
   const [items, setItems] = useState([
-    { id: 0, title: 'Reading', description: 'Read one book' },
+    { id: 0, title: 'Reading', description: 'Read one book', state: 'todo' },
+    {
+      id: 1,
+      title: 'Reading1',
+      description: 'Read one book2',
+      state: 'in-progress',
+    },
+    {
+      id: 2,
+      title: 'Reading2',
+      description: 'Read one book1',
+      state: 'in-progress',
+    },
   ]);
+
+  const stateValues = [
+    { value: 'All', text: 'All' },
+    { value: 'todo', text: 'ToDo' },
+    { value: 'in-progress', text: 'In progress' },
+    { value: 'done', text: 'Done' },
+  ];
+
+  const handleChange = (event) => {
+    setCurrentState(event.target.value);
+  };
 
   return (
     <>
-      <Typography className={classes.main__title}>ToDo List</Typography>
+      <Container className={classes.wrapper}>
+        <Typography className={classes.main__title}>ToDo List</Typography>
+        <Select
+          className={classes.selectState}
+          labelId="select-state"
+          value={currentState}
+          onChange={handleChange}
+        >
+          {stateValues.map((item) => {
+            return <MenuItem value={item.value}>{item.text}</MenuItem>;
+          })}
+        </Select>
+      </Container>
+
       <Container>
-        {items.map((item) => {
-          return <CardTodo key={item.id} todo={item} />;
-        })}
+        {items
+          .filter((todoItem) =>
+            currentState === 'All' ? todoItem : todoItem.state === currentState,
+          )
+          .map((item) => {
+            return <CardTodo key={item.id} todo={item} />;
+          })}
       </Container>
       <AddForm addItem={setItems} />
     </>
